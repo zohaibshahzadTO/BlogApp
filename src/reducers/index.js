04d5df081f4511c723b1e48b567c9ff3f3e73def
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-04-05 18:30:47 -0400

    Project Setup Complete

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import BooksReducer from './reducers_book';
+import ActiveBook from './reducer_active_book';
+
+const rootReducer = combineReducers({
+  
+});
+
+export default rootReducer;
