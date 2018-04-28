f664288cf5f746282827c406e4e7f412205bb365
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-04-28 19:36:02 -0400

    Updates
    - Discussed React-Router Architecture
    - Started boilerplate React-Router in BlogApp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducers_book';
-import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  
+
 });
 
 export default rootReducer;
